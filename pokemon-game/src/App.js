91190b3fbb1a7239911a669cb61fe21ad0270180
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2021-01-27 17:16:10 +0200

    Исправление
    Переделал после того как увидел ссылку на ТЗ

--- pokemon-game/src/App.js
+++ pokemon-game/src/App.js
@@ -1,35 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import HeaderBlock from './components/headerBlock/index.js';
+import Header from './components/Header/index.js';
+import Layout from './components/Layout/index.js';
+import Footer from './components/Footer/index.js';
 
-const AppList = () => {
-  const items = ['item 1', 'item 2'];
-  return (
-    <ul>
-      {
-        items.map(item => <li>{item}</li>)
-      }
-    </ul>
-  );
-};
+import bg1 from './components/Layout/images/bg1.jpg';
+import bg3 from './components/Layout/images/bg3.jpg';
 
-const AppHeader = () => {
-  return (
-    <h1 className="header">Hello world!</h1>
-  );
-}
 
 const App = () => {
   return (
-    <>
-      <HeaderBlock title="React.JS is awesome!" description ="That's my first React-rendered HTML file!" />
-      {/* <AppHeader />
-      <AppList /> */}
+    <>     
+      <Header title="This is title" description="This is Description!" />
+      <Layout urlBg={bg1} title="Title of 1-st section" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat quas saepe aspernatur, iste facilis enim officia laborum, debitis eius pariatur exercitationem neque. Quisquam natus nobis illum eos ipsum quis hic."/>
+      <Layout colorBg="#fff" title="Title of 2-ond section" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat quas saepe aspernatur, iste facilis enim officia laborum, debitis eius pariatur exercitationem neque. Quisquam natus nobis illum eos ipsum quis hic." />
+      <Layout urlBg={bg3} title="Title of 3-rd section" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat quas saepe aspernatur, iste facilis enim officia laborum, debitis eius pariatur exercitationem neque. Quisquam natus nobis illum eos ipsum quis hic." />
+      <Footer />
     </>
   );
 }
-
 const el = <App />;
 
 ReactDOM.render(el, document.getElementById('root'));
