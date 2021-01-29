778b3a368d79d731e52fcb79943a4ecb1b52ad1e
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2021-01-29 17:05:24 +0200

    Выполнил третье ДЗ
    Постоянно ставил на паузу... пропустил момент куда ты вставил JSON, и потом втыкал сидел полчаса. Очень быстро рассказываешь. Вроде выполнил всё как надо. Обрати внимание пожалуйста на CSS файлы, потому что не очень уверен что правильно вставил стили

--- pokemon-game/src/components/Layout/index.js
+++ pokemon-game/src/components/Layout/index.js
@@ -1,7 +1,6 @@
 import s from './style.module.css';
 
-const Layout = ({id, title, description, colorBg, urlBg}) => {
-    console.log(`..${urlBg}`);
+const Layout = ({id, title, colorBg, urlBg, children}) => {
     return (
         
         <section className={s.root} id={id} style={colorBg ? {backgroundColor:colorBg} : {background:`url(${urlBg})`}}>
@@ -11,8 +10,8 @@ const Layout = ({id, title, description, colorBg, urlBg}) => {
                         <h3>{title}</h3>
                         <span className={s.separator}></span>
                     </div>
-                    <div className={s.desc, s.full}>
-                        <p>{description}</p>
+                    <div className={`${s.full} ${s.desc}`}>
+                        {children}
                     </div>
                 </article>
             </div>
